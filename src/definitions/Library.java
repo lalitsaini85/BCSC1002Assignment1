88d816d9e6bb3402efa9bc6894fd39fb526d320b
LALIT <lalit.saini_cs19@gla.ac.in>
2020-08-25 22:13:08 +0530

    add constructor method for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -8,4 +8,8 @@ package definitions;
 
 public class Library {
     private Book[] currentlyAvailableBooks;
+
+    public Library(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
 }
