65dd747f5492e61955a429a7f7c53fdeb9d22b8f
LALIT <lalit.saini_cs19@gla.ac.in>
2020-08-26 21:39:46 +0530

    add getter and setter method to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -11,4 +11,36 @@ public class Student {
     private long universityRollNumberOfStudent;
     private int numberOfBooksIssuedByStudent;
     private Book[] allNamesOfBooksIssuedByStudent;
+
+    public String getNameOfStudent() {
+        return nameOfStudent;
+    }
+
+    public void setNameOfStudent(String nameOfStudent) {
+        this.nameOfStudent = nameOfStudent;
+    }
+
+    public long getUniversityRollNumberOfStudent() {
+        return universityRollNumberOfStudent;
+    }
+
+    public void setUniversityRollNumberOfStudent(long universityRollNumberOfStudent) {
+        this.universityRollNumberOfStudent = universityRollNumberOfStudent;
+    }
+
+    public int getNumberOfBooksIssuedByStudent() {
+        return numberOfBooksIssuedByStudent;
+    }
+
+    public void setNumberOfBooksIssuedByStudent(int numberOfBooksIssuedByStudent) {
+        this.numberOfBooksIssuedByStudent = numberOfBooksIssuedByStudent;
+    }
+
+    public Book[] getAllNamesOfBooksIssuedByStudent() {
+        return allNamesOfBooksIssuedByStudent.clone();
+    }
+
+    public void setAllNamesOfBooksIssuedByStudent(Book[] allNamesOfBooksIssuedByStudent) {
+        this.allNamesOfBooksIssuedByStudent = allNamesOfBooksIssuedByStudent;
+    }
 }
--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,7 +6,7 @@
  * */
 package execution;
 
-import definitions.Book;
+import java.util.Scanner;
 
 public class FrontDesk {
 
@@ -16,9 +16,30 @@ public class FrontDesk {
     private static final int EXIT = 4;
 
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        //int studentInput;
-        Book book = new Book();
-        System.out.println(book);
+        Scanner scanner = new Scanner(System.in);
+        int studentInput;
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter your choice from 1 to 4: ");
+            studentInput = scanner.nextInt();
+            switch (studentInput) {
+                case ISSUE_NEW_BOOK:
+                    System.out.println("Enter the name of the book that you want: ");
+                    break;
+                case RETURN_PREVIOUSLY_ISSUES_BOOK:
+                    break;
+                case SHOW_ALL_MY_ISSUES_BOOKS:
+                    break;
+                case EXIT:
+                    break;
+                default:
+                    System.out.println("WRONG CHOICE!!");
+            }
+        } while (studentInput != EXIT);
     }
 }
